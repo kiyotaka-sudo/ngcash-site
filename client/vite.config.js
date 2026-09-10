31c65c6a67be854693736dad248467ad463a09ef
mbargaernest <mbargaernest80@gmail.com>
2026-09-10 10:53:59 +0100

    Configure GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ngcash-site/', // Nécessaire pour GitHub Pages
 })
